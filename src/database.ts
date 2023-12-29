f263e7eeb9a016d3dde56a40befa010c5784b66b
igor.ribeiro <igorapalvesribeiro@gmail.com>
2023-12-29 10:36:58 -0300

    database - feat: first database configurations

---
+++ src/database.ts
@@ -0,0 +1,15 @@
+import Knex  from 'knex';
+
+export const config = {
+    client: 'sqlite',
+    connection: {
+        filename: './db/app.db'
+    },
+    useNullAsDefault: true,
+    migrations: {
+        extension: 'ts',
+        directory: './db/migrations',
+    }
+};
+
+export const knex = Knex(config);
